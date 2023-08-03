2189dbaa11fa2eafcd35d7876e4fa611c710aac2
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-08-03 11:30:21 -0300

    Learning Spring

--- Spring/api/src/main/java/br/com/projeto/api/models/Pessoa.java
+++ Spring/api/src/main/java/br/com/projeto/api/models/Pessoa.java
@@ -1,6 +1,18 @@
 package br.com.projeto.api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
     private String name;
     private String age;
 
@@ -17,5 +29,12 @@ public class Pessoa {
 
     public void setAge(String age) {
         this.age = age;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 }
